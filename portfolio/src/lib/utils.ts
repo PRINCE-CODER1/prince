633884b3c0e99dedead50c219c2b9472a8e42228
prince <princek002003@gmail.com>
2026-07-20 14:03:16 +0530

    ttf

--- portfolio/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-
-// Simple cn utility without tailwind-merge to reduce dependencies
-export function cn(...inputs: ClassValue[]) {
-  return clsx(inputs);
-}
